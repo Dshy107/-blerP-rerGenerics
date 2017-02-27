31f69b1d4a0bab91af02893c315312fdff937ca0
Dshy <KortSkg@KORTSKG-PC>
2017-02-27 14:39:22 +0100

    Har lavet equal, icomparable og icomparar

--- ÆblerPærerGenerics/Program.cs
+++ ÆblerPærerGenerics/Program.cs
@@ -19,23 +19,72 @@ namespace ÆblerPærerGenerics
             æbleBeholdning.Add(new Æbler() { Navn = "Guldborg", Lager = 100, Pris = 9.0M });
             æbleBeholdning.Add(new Æbler() { Navn = "LøgÆble", Lager = 4, Pris = 5.0M });
 
+            foreach (var item in æbleBeholdning)
+            {
+                Console.WriteLine($"navn {item.Navn} pris {item.Pris}");
+            }
+
+            æbleBeholdning.Sort();
+            Console.WriteLine("Pris efter sortering: ");
+            foreach (var item in æbleBeholdning)
+            {
+                Console.WriteLine($"navn {item.Navn} pris {item.Pris}");
+            }
+            Console.WriteLine("Lager efter sortering igen: ");
+            æbleBeholdning.Sort(Æbler.sortLagerDescending());
+            foreach (var item in æbleBeholdning)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("Pris efter sortering stigene: ");
+            æbleBeholdning.Sort(Æbler.sortPrisAccending());
+            foreach (var item in æbleBeholdning)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("Pris efter sortering faldende: ");
+            æbleBeholdning.Sort(Æbler.sortPrisDescending());
+            foreach (var item in æbleBeholdning)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            //var pærerBeholdning = new List<Pærer>();
+            //pærerBeholdning.Add(new Pærer() { Navn = "Conference", Lager = 75M, Pris = 5M });
+            //pærerBeholdning.Add(new Pærer() { Navn = "Concorde", Lager = 33M, Pris = 12M });
+            //pærerBeholdning.Add(new Pærer() { Navn = "Glorød Williams", Lager = 10, Pris = 15M });
+            //pærerBeholdning.Add(new Pærer() { Navn = "Noveau Poiteau", Lager = 5M, Pris = 22M });
+            //pærerBeholdning.Add(new Pærer() { Navn = "Tongre", Lager = 7M, Pris = 21M });
 
 
-            var pærerBeholdning = new List<Pærer>();
-            pærerBeholdning.Add(new Pærer() { Navn = "Conference", Lager = 75M, Pris = 5M });
-            pærerBeholdning.Add(new Pærer() { Navn = "Concorde", Lager = 33M, Pris = 12M });
-            pærerBeholdning.Add(new Pærer() { Navn = "Glorød Williams", Lager = 10, Pris = 15M });
-            pærerBeholdning.Add(new Pærer() { Navn = "Noveau Poiteau", Lager = 5M, Pris = 22M });
-            pærerBeholdning.Add(new Pærer() { Navn = "Tongre", Lager = 7M, Pris = 21M });
+            //decimal værdiTotalÆbler = FrugtHandler.CalculateSumÆbler(æbleBeholdning);
+            //Console.WriteLine("Så meget er æblebeholdningen værd : " + værdiTotalÆbler );
 
+            //decimal værdiTotalPærer = FrugtHandler.CalculateSumPærer(pærerBeholdning);
+            //Console.WriteLine("Så meget er pærerbeholdningen værd: " + værdiTotalPærer);
 
-            decimal værdiTotalÆbler = FrugtHandler.CalculateSumÆbler(æbleBeholdning);
-            Console.WriteLine("Så meget er æblebeholdningen værd : " + værdiTotalÆbler );
+            var æble1 = new Æbler()
+            {
+                Navn = "Cox Orange",
+                Lager = 22,
+                Pris = 10.0M
+            };
+            var æble2 = new Æbler()
+            {
+                Navn = "Cox Orange",
+                Lager = 22,
+                Pris = 10.0M
+            };
+            Æbler æble3 = æble2;
+            var æble4 = new Æbler()
+            {
+                Navn = "Cox Orange",
+                Lager = 1,
+                Pris = 25.0M
+            };
 
-            decimal værdiTotalPærer = FrugtHandler.CalculateSumPærer(pærerBeholdning);
-            Console.WriteLine("Så meget er pærerbeholdningen værd: " + værdiTotalPærer);
-
-
+            Console.WriteLine("æble1 equals æble2: " + æble1.Equals(æble2));
+            Console.WriteLine("æble3 equals æble2: " + æble3.Equals(æble1));
+            Console.WriteLine("æble1 equals æble4: " + æble1.Equals(æble4));
             Console.ReadLine();
         }
     }
--- ÆblerPærerGenerics/Æbler.cs
+++ ÆblerPærerGenerics/Æbler.cs
@@ -6,11 +6,109 @@ using System.Threading.Tasks;
 
 namespace ÆblerPærerGenerics
 {
-    public class Æbler
+    public class Æbler : IComparable<Æbler>
     {
         public string Navn { get; set; }
         public decimal Pris { get; set; }
         public int Lager { get; set; }
+
+        public override bool Equals(object obj)
+        {
+
+            Æbler æble = obj as Æbler;
+
+            if (this.Navn == æble.Navn && this.Pris == æble.Pris)
+                return true;
+            else
+                return false;
+            //return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return this.Navn.GetHashCode() ^ this.Pris.GetHashCode() ^ this.Lager.GetHashCode();
+
+        }
+
+        public int CompareTo(Æbler other)
+        {
+            if (this.Pris < other.Pris)
+                return -1;
+            else if (this.Pris == other.Pris)
+                return 0;
+            else
+                return 1;
+            //throw new NotImplementedException();
+        }
+
+        private class sortLagerDown : IComparer<Æbler>
+        {
+            public int Compare(Æbler x, Æbler y)
+            {
+                if (x.Lager > y.Lager)
+                    return 1;
+                else if (x.Lager == y.Lager)
+                    return 0;
+                else
+                    return -1;
+                //throw new NotImplementedException();
+            }
+        }
+        private class sortPriserUp : IComparer<Æbler>
+        {
+            public int Compare(Æbler x, Æbler y)
+            {
+                if (x.Pris < y.Pris)
+                    return 1;
+                else if (x.Pris == y.Pris)
+                    return 0;
+                else
+                    return -1;
+            }
+        }
+
+        private class sortPriserDown : IComparer<Æbler>
+        {
+            public int Compare(Æbler x, Æbler y)
+            {
+                if (x.Pris > y.Pris)
+                    return 1;
+                else if (x.Pris == y.Pris)
+                    return 0;
+                else
+                    return -1;
+            }
+        }
+
+        private class sortPriserogNavnDown : IComparer<Æbler>
+        {
+            public int Compare(Æbler x, Æbler y)
+            {
+                if (x.Pris > y.Pris)
+                    return 1;
+                else if (x.Pris == y.Pris)
+                    return 0;
+                else
+                    return -1;
+            }
+        }
+
+        public static IComparer<Æbler> sortLagerDescending()
+        {
+            return new sortLagerDown();
+        }
+
+        public static IComparer<Æbler> sortPrisAccending()
+        {
+            return new sortPriserUp();
+        }
+        public static IComparer<Æbler> sortPrisDescending()
+        {
+            return new sortPriserDown();
+        }
+        public override string ToString()
+        {
+            return $"Navn {this.Navn} pris {this.Pris} lager {this.Lager}";
+        }
     }
 
 }
